1d8371254cc80e5dab4c908203824d8ef42045ad
AlejandroPS <alejandro@example.com>
2025-04-30 12:00:01 +0200

    cambiando el codigo v2.0

---
+++ src/test/java/com/mycompany/practica6/CCuentaTest.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.practica6;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alexm
+ */
+public class CCuentaTest {
+    
+    public CCuentaTest() {
+    }
+
+    @Test
+    public void testIngresar() {
+        System.out.println("ingresar");
+        double cantidad = 5.0;
+        CCuenta instance = new CCuenta();
+        int expResult = 0;
+        int result = instance.ingresar(cantidad);
+        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+    }
+        @Test
+    public void testIngresar1() {
+        System.out.println("ingresar");
+        double cantidad = -3.0;
+        CCuenta instance = new CCuenta();
+        int expResult = 2;
+        int result = instance.ingresar(cantidad);
+        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+    }
+        @Test
+    public void testIngresar2() {
+        System.out.println("ingresar");
+        double cantidad = -5.0;
+        CCuenta instance = new CCuenta();
+        int expResult = 1;
+        int result = instance.ingresar(cantidad);
+        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+    }
+
+//    @Test
+//    public void testRetirar() {
+//        System.out.println("retirar");
+//        double cantidad = 0.0;
+//        CCuenta instance = new CCuenta();
+//        instance.retirar(cantidad);
+//        fail("The test case is a prototype.");
+//    }
+    
+}
